75a77dec8cb8ccb89b8942f102733383a2b948ca
rckmnds <81979401+rckmnds@users.noreply.github.com>
2022-06-14 22:03:32 -0300

    Create VM

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.24.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "lab-devops-cloud-7aso-123"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
